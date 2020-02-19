cd705823ed26672cc1575cda4543fbba30d271c0
hosam faour <hosamf1992@gmail.com>
2020-02-19 20:13:29 +0200

    Updates

---
+++ precache-manifest.1772b801bbaf85652035e54ad5da13dd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604e5ac3a535357d319e14fa9aa11c47",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/index.html"
+  },
+  {
+    "revision": "ced213f90931aeac49d9",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/css/main.4a26aac5.chunk.css"
+  },
+  {
+    "revision": "9e8b6aa73f4fb9b01f54",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/js/2.9ddafd44.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/js/2.9ddafd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced213f90931aeac49d9",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/js/main.dac892df.chunk.js"
+  },
+  {
+    "revision": "6f43265e5d6191872796",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/js/runtime-main.a376482c.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "6dfe30c39faa9e3983cc8e417aa50338",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/avatar-aden.6dfe30c3.jpg"
+  },
+  {
+    "revision": "d1d950e09ee94a2bbdd4627e65143cbc",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/avatar-aiysha.d1d950e0.jpg"
+  },
+  {
+    "revision": "60ff8909e4d598e25c144f0b47d988e3",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/avatar-arthur.60ff8909.jpg"
+  },
+  {
+    "revision": "15bbd826f87c68cd85e98b016ff4077f",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/bg-layers.15bbd826.svg"
+  },
+  {
+    "revision": "e45a3d0d8de698bfbea8ddc00225fd8e",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/bg-pattern-home-1.e45a3d0d.svg"
+  },
+  {
+    "revision": "2fd804c4ec4e139d392e43c599bd277b",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/bg-pattern-home-2.2fd804c4.svg"
+  },
+  {
+    "revision": "458c9fcdfef1f53beea9819bd3a2b4e8",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/bg-pattern-home-5.458c9fcd.svg"
+  },
+  {
+    "revision": "fe4f785a6475f2ea47465eb9a6fcda38",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/bg-pattern.fe4f785a.svg"
+  },
+  {
+    "revision": "a87ab6a881ff4075af32eaa1399e4774",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/contact.a87ab6a8.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/facebook.7febcff9.svg"
+  },
+  {
+    "revision": "1ece178695707c353a82b30a19b11d2e",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/icon-chart.1ece1786.svg"
+  },
+  {
+    "revision": "6cb0f5760e3e8a0f4567755fb5250b32",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/icon-cog.6cb0f576.svg"
+  },
+  {
+    "revision": "05759150ed61077a65185636252594d7",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/icon-person.05759150.svg"
+  },
+  {
+    "revision": "efcf9a22216f31e99d42af206a8227a9",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/icon-quotes.efcf9a22.svg"
+  },
+  {
+    "revision": "d99f6328cbfa9c4ef459769d8e18357c",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/linkedin.d99f6328.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/%E2%80%8Bhttps://hosamf1992.github.io/Test-App/static/media/twitter.cb4fea0b.svg"
+  }
+]);
